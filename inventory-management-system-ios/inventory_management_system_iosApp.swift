416981210a6a75a05d6182ce9c3fe1d478a0c996
HanaokaY <okayu5757@ezweb.ne.jp>
2024-05-03 10:32:26 +0900

    first

---
+++ inventory-management-system-ios/inventory_management_system_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  inventory_management_system_iosApp.swift
+//  inventory-management-system-ios
+//
+//  Created by hanaoka yudai on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct inventory_management_system_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
